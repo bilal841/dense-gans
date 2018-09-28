14b740ef2c699cb270878b4717d2728e64eaa770
Thalles Silva <thalles753@gmail.com>
2018-09-28 10:34:06 -0300

    added configs.yml file

--- preprocessing/read_data.py
+++ preprocessing/read_data.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 
 def tf_record_parser(record):
     keys_to_features = {
+        #"image": tf.FixedLenFeature((), tf.string, default_value=""),
         'annotation': tf.FixedLenFeature([], tf.string),
         "height": tf.FixedLenFeature((), tf.int64),
         "width": tf.FixedLenFeature((), tf.int64)
@@ -9,12 +10,14 @@ def tf_record_parser(record):
 
     features = tf.parse_single_example(record, keys_to_features)
 
-    annotation = tf.decode_raw(features['annotation_raw'], tf.uint8)
+    #image = tf.decode_raw(features['image'], tf.uint8)
+    annotation = tf.decode_raw(features['annotation'], tf.uint8)
 
     height = tf.cast(features['height'], tf.int32)
     width = tf.cast(features['width'], tf.int32)
 
     # reshape input and annotation images
+    #image = tf.reshape(image, (height, width, 3), name="image_reshape")
     annotation = tf.reshape(annotation, (height,width,1), name="annotation_reshape")
     annotation = tf.to_float(annotation)
 
